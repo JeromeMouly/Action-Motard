fdb63ec810d137d22bb2c35f0a2c1e5083e7108f
JeromeMouly <mouly.jerome@gmail.com>
2016-04-29 13:51:06 +0200

    Correction bug responsive menu & fixed-navbar

--- js/script.js
+++ js/script.js
@@ -17,36 +17,9 @@ $(document).ready(function() {
 		});
 	});
 
-// Create a clone of the menu, right next to original.
-$('.menu-attached').addClass('original').clone().insertAfter('.menu-attached').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
-
-scrollIntervalID = setInterval(stickIt, 10);
-
-
-function stickIt() {
-
-  var orgElementPos = $('.original').offset();
-  orgElementTop = orgElementPos.top;               
-
-  if ($(window).scrollTop() >= (orgElementTop)) {
-    // scrolled past the original position; now only show the cloned, sticky element.
-
-    // Cloned element should always have same left position and width as original element.     
-    orgElement = $('.original');
-    coordsOrgElement = orgElement.offset();
-    leftOrgElement = coordsOrgElement.left;  
-    widthOrgElement = orgElement.css('width');
-    $('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
-    $('.original').css('visibility','hidden');
-  } else {
-    // not scrolled past the menu; only show the original menu.
-    $('.cloned').hide();
-    $('.original').css('visibility','visible');
-  }
-}
-
 //Dropdown
-$('.dropdown-button').dropdown({
+$(document).ready(function(){
+  $('.dropdown-button').dropdown({
       inDuration: 300,
       outDuration: 225,
       constrain_width: false, // Does not change width of dropdown to that of the activator
@@ -54,8 +27,8 @@ $('.dropdown-button').dropdown({
       gutter: 0, // Spacing from edge
       belowOrigin: false, // Displays dropdown below the button
       alignment: 'left' // Displays dropdown with edge aligned to the left of button
-    }
-  );
+    });
+});
 
 // Modal Inscrire
 $(document).ready(function(){
